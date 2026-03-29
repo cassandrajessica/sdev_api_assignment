e66e33b0180f32dd38af123d90518ced0cf019e7
cj <cj@Mac.lan>
2026-03-29 13:21:03 -0500

    updated api to integrate with postgres

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 import express from 'express';
-import sqlite3 from 'sqlite3';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
 
-// Creating the database
-const db = new sqlite3.Database('products.db');
+// connecting db
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+});
 
-db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        price REAL
-        )`);
-})
+// creating table 
+pool.query(`
+    CREATE TABLE IF NOT EXISTS products (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    price REAL
+    )
+`)
+.then(() => console.log('Table success'))
+.catch(err => console.error('Table error:', err));
 
 // ROOT END POINT
 app.get('/', (req, res) => {
@@ -23,23 +34,27 @@ app.get('/', (req, res) => {
 });
 
 // CREATE PRODUCTS END POINT
-app.post('/create-products', (req, res) => {
+app.post('/create-products', async (req, res) => {
     const { name, price } = req.body;
-    const sql = 'INSERT INTO products (name,price) VALUES(?, ?)';
-
-    db.run(sql, [name, price], function(err) {
-        if(err) return res.status(500).json({ error: err.message });
-        res.status(201).json({ id: this.lastID, name, price });
-    });
-
+    try{
+        const result = await pool.query(
+            'INSERT INTO products (name, price) VALUES($1, $2) RETURNING *',
+            [name, price]
+        );
+        res.status(201).json(result.rows[0]);
+    }catch(err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 // GET ALL PRODUCTS END POINT
-app.get('/get-products', (req, res) => {
-    db.all('SELECT * FROM products', [], (err, rows) => {
-        if(err) return res.status(500).json({ error: err.message });
-        res.json(rows);
-    });
+app.get('/get-products', async (req, res) => {
+    try{
+        const result = await pool.query('SELECT * FROM products');
+        res.json(result.rows);
+    }catch(err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.listen(PORT, () => {
